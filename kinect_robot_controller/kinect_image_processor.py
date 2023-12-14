d4e44f7371116d05a97277bfd7f992a86690004a
xalpol12 <xalpol12@gmail.com>
2023-12-14 20:29:09 +0100

    create threshold mask that detects images in certain range

--- kinect_robot_controller/kinect_image_processor.py
+++ kinect_robot_controller/kinect_image_processor.py
@@ -7,7 +7,7 @@ import numpy as np
 
 MIN_THRESHOLD_TRACKBAR_VALUE = 0
 MAX_THRESHOLD_TRACKBAR_VALUE = 200
-DEPTH_DETECTION_THRESHOLD = 20
+DEPTH_DETECTION_THRESHOLD = 40
 
 def on_detection_threshold_change(value):
     global DEPTH_DETECTION_THRESHOLD
@@ -37,24 +37,25 @@ class KinectImageProcessor(Node):
         
         self.rgb_image = np.array(0)
         self.depth_image = np.array(0)
+        self.thresh_away = np.array(0)
         self.threshed_depth_image = np.array(0)
 
         self.rgb_window = Window("rgb")
         self.depth_window = Window("threshed_depth")
-        self.depth_window.create_trackbar("threshold", MIN_THRESHOLD_TRACKBAR_VALUE, 
-                                          MAX_THRESHOLD_TRACKBAR_VALUE, on_detection_threshold_change)
+        # self.depth_window.create_trackbar("threshold", MIN_THRESHOLD_TRACKBAR_VALUE, MAX_THRESHOLD_TRACKBAR_VALUE, on_detection_threshold_change)
 
     def rgbd_callback(self, rgbd_image: Image):
         self.rgb_image = self.cv_bridge.imgmsg_to_cv2(rgbd_image, 'rgba8')[:, :, 0:3]
         self.depth_image = self.cv_bridge.imgmsg_to_cv2(rgbd_image, 'rgba8')[:, :, 3]
-        _, self.threshed_depth_image = cv2.threshold(self.depth_image, DEPTH_DETECTION_THRESHOLD, 
-                                                     255, cv2.THRESH_BINARY_INV)
+        self.depth_image = cv2.blur(self.depth_image, (7, 7))
+        _, thresh_away = cv2.threshold(self.depth_image, DEPTH_DETECTION_THRESHOLD, 255, cv2.THRESH_BINARY_INV)
+        _, thresh_close = cv2.threshold(self.depth_image, 2, 255, cv2.THRESH_BINARY)
+        self.threshed_depth_image = cv2.bitwise_xor(thresh_close, thresh_away)
         self.update_windows()
-        print(DEPTH_DETECTION_THRESHOLD)
 
     def update_windows(self):
-        self.rgb_window.update_image(self.rgb_image)
         self.depth_window.update_image(self.threshed_depth_image)
+        self.rgb_window.update_image(self.rgb_image)
 
 
 def main(args=None):
