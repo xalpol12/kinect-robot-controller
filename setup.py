13a0f37b18fadda7f7073fce928e7c1395b0df08
xalpol12 <xalpol12@gmail.com>
2023-12-28 13:49:52 +0100

    update setup and package info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'kinect_robot_controller'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,8 +15,8 @@ setup(
     zip_safe=True,
     maintainer='dawidexpompa',
     maintainer_email='xalpol12@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Image detection using Kinect v1 with OpenCV library',
+    license='BSD',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
